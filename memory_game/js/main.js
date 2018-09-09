1c898b17edfc9973b62e62c3a6327db578384352
albert lewis <deltin00@gmail.com>
2018-09-09 15:44:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,32 @@
 console.log("Up and running!");
 
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+
+];
 
 var cardsInPlay = [];
 
@@ -19,10 +44,13 @@ console.log("Sorry, try again.");
  
  function flipCard(cardId) {
 	// body...
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
+
+	console.log(card.cardImage);
+	console.log(card.suit);
 
 }
 
